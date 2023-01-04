9c0c739dbeed2bd19fd661984dd8e49edb08e7a9
ST0PL <117662221+ST0PL@users.noreply.github.com>
2023-01-05 00:59:07 +0700

    Create User.cs

---
+++ STGramApi/MessageModel/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace STGramApi.MessageModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public bool Is_bot { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Username { get; set; }
+        public string Language_code { get; set; }
+        public bool Is_premium { get; set; }
+        public bool Added_to_attachment_menu { get; set; }
+        public bool Can_join_groups { get; set; }
+        public bool Can_read_all_group_messages { get; set; }
+        public bool Supports_inline_queries { get; set; }
+    }
+}
