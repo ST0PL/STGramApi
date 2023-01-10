c04671bed85532302e4220b131a3fb5f31181f04
ST0PL <117662221+ST0PL@users.noreply.github.com>
2023-01-11 00:34:58 +0700

    Create MaskPosition.cs

---
+++ STGramApi/MessageModel/MaskPosition.cs
@@ -0,0 +1,10 @@
+namespace STGramApi.MessageModels
+{
+    class MaskPosition
+    {
+        public string Point { get; set; }
+        public float X_shift { get; set; }
+        public float Y_shift { get; set; }
+        public float Scale { get; set; }
+    }
+}
