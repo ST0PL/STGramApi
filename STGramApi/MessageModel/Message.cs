b89b861300b3f4dbddc6196fdca770aff605b385
ST0PL <117662221+ST0PL@users.noreply.github.com>
2023-01-11 00:35:59 +0700

    Update Message.cs

--- STGramApi/MessageModel/Message.cs
+++ STGramApi/MessageModel/Message.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using STGramApi.MessageModel.ReplyMarkup;
 
-namespace STGramApi.MessageModel
+namespace STGramApi.MessageModels
 {
     public class Message
     {
         public long Message_id { get; set; }
         public Chat Chat { get; set; } = new Chat();
+        public Chat Sender_chat { get; set; } = new Chat();
         public From From { get; set; } = new From();
+        public User Forward_from { get; set; } = new User();
+        public Chat Forward_from_chat { get; set; } = new Chat();
+        public long Forward_from_message_id { get; set; }
+        public string Forward_signature { get; set; }
+        public string Forward_sender_name { get; set; }
+        public long Forward_date { get; set; }
+        public bool Is_topic_message { get; set; }
+        public bool Is_automatic_forward { get; set; }
+        public User Via_bot { get; set; } = new User();
+        public long Edit_date { get; set; }
+        public bool Has_protected_content { get; set; }
+        public Audio Audio { get; set; } = new Audio();
         public string Text { get; set; }
         public long Date { get; set; }
         public Document Document { get; set; } = new Document();
